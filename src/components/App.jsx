a415b19d00c35d6850570b660344985b36dc4dcf
Dheemanth Narasimha <dheemanth.simha01@gmail.com>
2021-07-06 19:14:09 +0530

    Complete phase 2 Mapped through notes list to render them on the home page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,17 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map((note) => {
+        return (
+          <Note title={note.title} content={note.content} key={note.key} />
+        );
+      })}
       <Footer />
     </div>
   );
